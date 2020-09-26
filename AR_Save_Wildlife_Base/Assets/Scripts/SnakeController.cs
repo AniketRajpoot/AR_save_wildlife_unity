3dfd21f28a902e7b2b603d6274d436df15731773
Aniket Singh Rajpoot <52102674+Doomguy16@users.noreply.github.com>
2020-09-27 02:22:25 +0530

    Added missing script

---
+++ AR_Save_Wildlife_Base/Assets/Scripts/SnakeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class SnakeController : MonoBehaviour
+{
+    private DetectedPlane detectedPlane;
+    public GameObject snakeHeadPrefab;
+    private GameObject snakeInstance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnSnake()
+    {
+        if (snakeInstance != null)
+        {
+            DestroyImmediate(snakeInstance);
+        }
+
+        Vector3 pos = detectedPlane.CenterPose.position;
+
+        // Not anchored, it is rigidbody that is influenced by the physics engine.
+        snakeInstance = Instantiate(snakeHeadPrefab, pos,
+                Quaternion.identity, transform);
+
+        // Pass the head to the slithering component to make movement work.
+        GetComponent<Slithering>().Head = snakeInstance.transform;
+    }
+
+    public void SetPlane(DetectedPlane plane)
+    {
+        detectedPlane = plane;
+        // Spawn a new snake.
+        SpawnSnake();
+    }
+}
